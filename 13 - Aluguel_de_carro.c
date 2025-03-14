30e45ed22bef587d5f03788acb53a7af92bd3f0f
Eliã Carlos Frazão Sá <eliac@acad.ifma.edu.br>
2025-03-14 12:12:58 -0300

    13 - Aluguel_de_carro

---
+++ 13 - Aluguel_de_carro.c
@@ -0,0 +1,26 @@
+/* Escreva um algoritmo que pergunte a quantidade de km percorridos por um carro alugado
+pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o
+preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por Km rodado. */
+
+#include <stdio.h>
+
+int main(){
+    int dia;
+    float km, valor;
+
+    printf("Informe a quantidade de quilometros rodados: ");
+    scanf("%f", &km); 
+
+    printf("\n");
+
+    printf("Informe a quantidade de dias ultilizado: ");
+    scanf("%d", &dia);
+
+    printf("\n");
+
+    valor = (dia * 60) + (km * 0.15);
+
+    printf("%2.f km rodados e %d dias, o valor a pagar sera:  R$%.2f", km, dia, valor);
+
+    return 0;
+}
