6b1ecc4d79da85b4e60dc5c20a000acebf04c9e0
Eliã Carlos Frazão Sá <eliac@acad.ifma.edu.br>
2025-04-01 13:54:44 -0300

    27 - calculo_de_dois_numeros

---
+++ 27 - calculo_de_dois_numeros.c
@@ -0,0 +1,31 @@
+/* Faça um algoritmo que leia dois valores inteiros A e B se os valores forem iguais deverá se somar os
+dois, caso contrário multiplique A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado
+para uma variável C e mostrar seu conteúdo na tela. */
+
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+
+    printf("Digite o valor de A: ");
+    scanf("%d", &a);
+
+    printf("\n");
+
+    printf("Digite o valor de B: ");
+    scanf("%d", &b);
+
+    printf("\n");
+
+    if (a == b){
+        c = a + b;
+        printf("Como %d e %d sao iguais, soma-se o resultado, ficando: %d", a, b, c);
+    }
+
+    else{
+        c = a * b;
+        printf("Como %d e %d sao diferentes, multiplica-se o resultado, ficando: %d", a, b, c);
+    }
+
+    return 0;
+}
